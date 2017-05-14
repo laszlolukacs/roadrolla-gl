47841356f35c70837b62f44cb8f9f92261d8fedb
Laszlo Lukacs <laci.28@outlook.com>
2017-05-15 01:24:30 +0200

    Major refactor of the camera

--- src/Camera.h
+++ src/Camera.h
@@ -4,32 +4,128 @@
 #include <GL/freeglut.h>
 #include "Vector.h"
 
+/* Structure for 3 dimensional OpenGL camera and its respective operations. */
 struct Camera
 {
+	/* Specifies the position of the observer. */
 	Vector eyePos;
-	Vector lookAt;
+
+	/* Specifies the `front` direction of the observer. */
+	Vector frontDir;
+
+	/* Specifies the `up` direction of the observer. */
 	Vector upDir;
 
+	/* The field of view, angle of view frustum, in degrees. */
 	float fov;
+
+	/* The distance of the frontal clipping pane from the observer, nearest view frustum plane. */
 	float nearClippingPane;
+
+	/* The distance of the rear clipping pane from the observer, farthest view frustum plane. */
 	float rearClippingPane;
+
+	/* The respective dimension of the viewport. */
 	int viewportLeft, viewportBottom, viewportWidth, viewportHeight;
 
-	Camera() {}
-
-	void setupProjection_gl() const
+	/**
+	 * Initializes a new instance of the Camera struct.
+	 */
+	Camera()
 	{
-		glViewport(viewportLeft, viewportBottom, viewportWidth, viewportHeight);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluPerspective(fov, float(viewportWidth) / float(viewportHeight), nearClippingPane, rearClippingPane);
+		this->eyePos = Vector(0.0f, 0.0f, 3.0f);
+		this->frontDir = Vector(0.0f, 0.0f, -1.0f);
+		this->upDir = Vector(0.0f, 1.0f, 0.0f);
+		this->fov = 90.0f;
+		this->nearClippingPane = 1.0f;
+		this->rearClippingPane = 45.0f;
+		this->viewportLeft = this->viewportBottom = 0;
+		this->viewportWidth = this->viewportHeight = 600;
 	}
 
-	void setupModelView_gl() const
+	/**
+	* Sets the GL_PROJECTION matrix of a 3D camera with the stored parameters.
+	*/
+	void setProjectionMatrix() const
 	{
+		glViewport(this->viewportLeft, this->viewportBottom, this->viewportWidth, this->viewportHeight);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(this->fov, float(this->viewportWidth) / float(this->viewportHeight), this->nearClippingPane, this->rearClippingPane);
+	}
+
+	/**
+	 * Sets the GL_MODELVIEW matrix of a 3D camera with the specified positions.
+	 */
+	void setModelViewMatrix() const
+	{
+		Vector lookAt = this->eyePos + this->frontDir;
+
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		gluLookAt(eyePos.x, eyePos.y, eyePos.z, lookAt.x, lookAt.y, lookAt.z, upDir.x, upDir.y, upDir.z);
+		gluLookAt(
+			this->eyePos.x, this->eyePos.y, this->eyePos.z,
+			lookAt.x, lookAt.y, lookAt.z,
+			this->upDir.x, this->upDir.y, this->upDir.z);
+	}
+
+	/**
+	 * Sets the viewport coordinates for the camera.
+	 *
+	 * @param viewportLeft The leftmost coordinate of the viewport.
+	 * @param viewportBottom The bottommost coordinate of the viewport.
+	 * @param viewportWidth The width of the viewport in pixels.
+	 * @param viewportHeight The height of the viewport in pixels.
+	 */
+	void setViewport(int viewportLeft, int viewportBottom, int viewportWidth, int viewportHeight)
+	{
+		this->viewportLeft = viewportLeft;
+		this->viewportBottom = viewportBottom;
+		this->viewportWidth = viewportWidth;
+		this->viewportHeight = viewportHeight;
+		this->setProjectionMatrix();
+	}
+
+	/**
+	 * Moves the camera to the specified direction.
+	 *
+	 * @param direction The direction to which the camera should be moved.
+	 */
+	void move(const Vector direction)
+	{
+		this->eyePos += direction;
+		this->setProjectionMatrix();
+	}
+
+	/**
+	 * Rotates the camera with the specified angles.
+	 *
+	 * @param yaw The yaw angle of the camera in degrees.
+	 * @param pitch The pitch angle of the camera in degrees.
+	 */
+	void rotate(float yaw, float pitch)
+	{
+		// TODO: use/implement math.clamp instead of plain branching to clamp pitch values
+		if (pitch > this->fov - 15.1f)
+		{
+			pitch = this->fov - 15.1f;
+		}
+
+		if (pitch < -1.0f * this->fov + 15.1f)
+		{
+			pitch = -1.0f * this->fov + 15.1f;
+		}
+
+		Vector frontDir;
+		float yawRadians = yaw * 3.1415926535f / 180.0f;
+		float pitchRadians = pitch * 3.1415926535f / 180.0f;
+
+		frontDir.x = cosf(yawRadians) * cosf(pitchRadians);
+		frontDir.y = sinf(pitchRadians);
+		frontDir.z = sinf(yawRadians) * cosf(pitchRadians);
+
+		this->frontDir = frontDir.normalize();
+		this->setProjectionMatrix();
 	}
 };
 
